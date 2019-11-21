66663faa7311a5376afd4770802fac5f45d090f3
syahirah <syahirah.suradi@gmail.com>
2019-11-21 13:35:19 +0800

    got the map function working in index.jsx

--- src/client/User.jsx
+++ src/client/User.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import tweets from './tweets';
+import Tweets from './tweets';
 
 class User extends React.Component {
     render() {
-        let name = tweets[0].user.name;
-        let username = tweets[0].user.screen_name;
+        let name = this.props.identity.name
+        let username = this.props.identity.screen_name
+        console.log(this.props.identity)
         return(
             <div>
                 <p>{name}</p>
-                <p>{username}</p>
+                <p>@{username}</p>
             </div>
         );
     }
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,46 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import tweets from './tweets';
+import Tweets from './tweets';
 import User from './User';
 
 class App extends React.Component {
   render() {
+    let allTweets = Tweets.map (eachTweet => {
+       return <User identity={eachTweet.user}/>
+    })
     return (
       <div>
-        <User />
+        {allTweets}
       </div>
     );
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-// class Tweet extends React.Component {
-//   render() {
-//         let allTweets = this.props.tweets.map (eachTweet =>
-//             <User identity={eachTweet.user}></User>
-//         )
-//         return (
-//             allTweets
-//     );
-//   }
-// }
-
-// class User extends React.Component {
-//     render () {
-//         let name = this.props.user.name
-//         let username = this.props.user.screen_name
-
-//         return (
-//             <div>
-//                 <p>{name}</p>
-//                 <p>@{username}</p>
-//             </div>
-//         )
-//     }
-// }
-
-// ReactDOM.render(
-//     <Tweet post={tweets} />, document.getElementById('app') );
